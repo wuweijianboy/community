0d0afef887d94ee7a804099556d50c79efc49b19
unknown <1075806554@qq.com>
2021-07-06 07:18:02 +0800

    index list

---
+++ src/main/java/life/wwj/community/dto/PublishDTO.java
@@ -0,0 +1,5 @@
+package life.wwj.community.dto;/**
+@author wwj
+@create 2021-07-05-22:17
+*/public class PublishDTO {
+}
---
+++ src/main/java/life/wwj/community/service/PublishService.java
@@ -0,0 +1,5 @@
+package life.wwj.community.service;/**
+@author wwj
+@create 2021-07-05-22:19
+*/public class PublishService {
+}
